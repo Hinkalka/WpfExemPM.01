b58d198d8910e90f991a56d364a63253b6e24c34
daniilsergeeg@gmail.com <daniilsergeeg@gmail.com>
2022-04-19 09:35:11 +0300

    Создание папок, добавление базы данных в десктопное приложение, добавление основных классов

---
+++ WpfExemPM.01/FolderClasses/Pages.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfExemPM._01.FolderClasses
+{
+    class Pages
+    {
+        public static Frame frame;
+    }
+}
